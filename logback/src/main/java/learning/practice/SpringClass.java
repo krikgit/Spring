212ecc981f1b41485b85bfb5ed3f0d931e02f8b3
Krik <krishnakamalmandal1995@gmail.com>
2020-08-26 09:27:00 +0530

    Adding Spring projects

---
+++ logback/src/main/java/learning/practice/SpringClass.java
@@ -0,0 +1,15 @@
+package learning.practice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpringClass {
+	
+	public final static Logger log = LoggerFactory.getLogger(SpringClass.class);
+	public static void main(String[] args) {
+		log.info("Hi from Spring class");
+		log.debug("Hello");
+		
+	}
+
+}
